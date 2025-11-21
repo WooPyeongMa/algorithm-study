feb3f5977c2141300e73a2c629a12543c2d025b2
chaehyunkIM <chaehhyun0803@pusan.ac.kr>
2025-11-22 02:10:06 +0900

    feat: 2025-11-23 baekjoon 18529 문제 풀이

---
+++ chaehyun/2025-11-23_baekjoon_18258.cpp
@@ -0,0 +1,57 @@
+//
+//  2025-11-23_baekjoon_18258.cpp
+//  algorithm
+//
+//  Created by 김채현 on 11/22/25.
+//
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<int> q;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int num;
+    
+    cin >> num;
+    
+    while(num--) {
+        string str;
+        
+        cin >> str;
+        
+        if(str == "push") {
+            int input;
+            cin >> input;
+            q.push(input);
+        }
+        else if(str == "pop") {
+            if(q.empty()) cout << -1 << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if(str == "size") cout << q.size() << '\n';
+        else if(str == "empty") {
+            if(q.empty()) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        }
+        else if(str == "front") {
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        }
+        else {
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+    
+    return 0;
+}
